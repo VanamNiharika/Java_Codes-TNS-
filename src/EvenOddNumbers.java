ed0a86690021ff6823460d048ff9f74bc65d5272
vanam <vanam@DESKTOP_NIHA_27>
2024-06-15 20:14:41 +0530

    EvenOddNumbers

---
+++ src/EvenOddNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EvenOddNumbers {
+	
+	public static void main (String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int number = scanner.nextInt();
+		if (number%2==0){
+
+			System.out.println(number +" is an even number");
+			
+		}else {
+
+			System.out.println(number + " an odd number");
+
+		}
+
+		scanner.close();
+	}
+}
+
