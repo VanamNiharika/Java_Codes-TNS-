ae55d1779815531ea5ca31ec858a94e47c2614f6
vanam <vanam@DESKTOP_NIHA_27>
2024-06-17 17:10:59 +0530

    ArrayOfObjectsImplementation

---
+++ src/arrayofobjects/Student.java
@@ -0,0 +1,11 @@
+package arrayofobjects;
+
+class Student{
+    public int roll_no;
+    public String name;
+    Student(int roll_no, String name)   // Parameterized Constructor
+    {
+        this.roll_no = roll_no;
+        this.name = name;
+    }
+}
