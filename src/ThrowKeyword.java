e3d1491b8c382d67eae9844a5c0c17987a121e7c
vanam <vanam@DESKTOP_NIHA_27>
2024-06-22 18:51:08 +0530

    Throw Keyword

---
+++ src/ThrowKeyword.java
@@ -0,0 +1,14 @@
+public class ThrowKeyword {
+    public static void main(String[] args) {
+        try {
+            int age = 20;
+            if (age < 18) {
+                throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+            } else {
+                System.out.println("Access granted - You are old enough!");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
